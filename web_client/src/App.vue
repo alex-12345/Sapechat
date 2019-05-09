8adff42cd6bed6e334654b2f0d0a0f1167e8471f
Alex Vinnikov <vin-nikov@yandex.ru>
2019-05-09 21:23:03 +0300

    add hidden-area in header

--- web_client/src/App.vue
+++ web_client/src/App.vue
@@ -1,9 +1,12 @@
 <template lang="pug">
-  .wrapper
+  .wrapper-header
     header
       #h-content
         #h-logo
-          button#switch
+          button#switch(
+            @click="switchShow = !switchShow"
+            
+          )
             img(src='./assets/static/images/switch.png', alt='Переключить')
           router-link#logo(to="/") SapeChat
         #h-search
@@ -18,12 +21,35 @@
           router-link.h-link(to="/messenger") Диалоги
           router-link.h-link(to="/people") Люди
           router-link.h-link(to="/logout") Выход
+      #hidden-area
+        #hidden-block(v-show = "switchShow")
+          #hidden-profile
+            a(href='')
+              img#me-img(src='files/user_img/8.jpg', alt='Me')
+            a#hidden-username(href='') Имя Фамилия
+            span#hidden-email username@example.com
+          #hidden-list
+            a(href='')
+              div
+                img(src='./assets/static/images/group_gray.png', alt='')
+              |        Создать конференцию
+            a(href='')
+              div
+                img(src='./assets/static/images/setting_gray.png', alt='')
+              |         Настройки
+            a(href='')
+              div
+                img(src='./assets/static/images/logout_gray.png', alt='')
+              |         Выход
+          span#hidden-version SapeChat v1.0
+
 </template>
 
 <script>
 export default {
   data(){
     return{
+      switchShow: false,
       linkMenu: [
         {title: 'Home', url: '/'},
       ]
