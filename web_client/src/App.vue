acedc7e681c3bb263ffb6df9fdce4fc215095e6e
Alex Vinnikov <vin-nikov@yandex.ru>
2019-05-09 23:51:23 +0300

    header modify

--- web_client/src/App.vue
+++ web_client/src/App.vue
@@ -15,40 +15,47 @@
             input(type='search', name='query', placeholder='Введите запрос...', required='', maxlength='40', autocomplete='off')
             button
               img(src='./assets/static/images/search_gray.png', alt='')
-        #h-links
+        #h-links(v-if="signin_pass")
           router-link.h-link(to="/im") Профиль
           router-link.h-link(to="/feed") Лента
           router-link.h-link(to="/messenger") Диалоги
           router-link.h-link(to="/people") Люди
           router-link.h-link(to="/logout") Выход
+        #h-links(v-else)
+          router-link.h-link(to="/signup") Регистрация
       #hidden-area
         #hidden-block(v-show = "switchShow")
-          #hidden-profile
-            a(href='')
-              img#me-img(src='files/user_img/8.jpg', alt='Me')
-            a#hidden-username(href='') Имя Фамилия
-            span#hidden-email username@example.com
-          #hidden-list
-            a(href='')
-              div
-                img(src='./assets/static/images/group_gray.png', alt='')
-              |        Создать конференцию
-            a(href='')
-              div
-                img(src='./assets/static/images/setting_gray.png', alt='')
-              |         Настройки
-            a(href='')
-              div
-                img(src='./assets/static/images/logout_gray.png', alt='')
-              |         Выход
+          .hidden-wrapper(v-if="signin_pass")
+            #hidden-profile
+              a(href='')
+                img#me-img(src='files/user_img/8.jpg', alt='Me')
+              a#hidden-username(href='') Имя Фамилия
+              span#hidden-email username@example.com
+            #hidden-list
+              a(href='')
+                div
+                  img(src='./assets/static/images/group_gray.png', alt='')
+                |        Создать конференцию
+              a(href='')
+                div
+                  img(src='./assets/static/images/setting_gray.png', alt='')
+                |         Настройки
+              router-link(to="/logout")
+                div
+                  img(src='./assets/static/images/logout_gray.png', alt='')
+                |         Выход
+          .hidden-wrapper(v-else)
+            #hidden-alert Войдите или зарегистриуйтесь
+
           span#hidden-version SapeChat v1.0
 
 </template>
 
 <script>
 export default {
-  data(){
-    return{
+  data( ) {
+    return {
+      signin_pass: true,
       switchShow: false,
       linkMenu: [
         {title: 'Home', url: '/'},
