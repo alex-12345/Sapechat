598d66f507167728edfb1717ed22996d69e44904
Alex Vinnikov <vin-nikov@yandex.ru>
2019-05-09 20:30:08 +0300

    creat header

--- web_client/src/App.vue
+++ web_client/src/App.vue
@@ -1,23 +1,37 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<template lang="pug">
+  .wrapper
+    header
+      #h-content
+        #h-logo
+          button#switch
+            img(src='./assets/static/images/switch.png', alt='Переключить')
+          router-link#logo(to="/") SapeChat
+        #h-search
+          form(action='search', method='get')
+          .search
+            input(type='search', name='query', placeholder='Введите запрос...', required='', maxlength='40', autocomplete='off')
+            button
+              img(src='./assets/static/images/search_gray.png', alt='')
+        #h-links
+          router-link.h-link(to="/im") Профиль
+          router-link.h-link(to="/feed") Лента
+          router-link.h-link(to="/messenger") Диалоги
+          router-link.h-link(to="/people") Люди
+          router-link.h-link(to="/logout") Выход
 </template>
 
 <script>
 export default {
-  name: 'App'
+  data(){
+    return{
+      linkMenu: [
+        {title: 'Home', url: '/'},
+      ]
+    }
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus">
+@import './assets/stylus/main.styl'
 </style>
--- web_client/src/router/index.js
+++ web_client/src/router/index.js
@@ -1,25 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'index',
-      component: HelloWorld
-    },
-    {
-      path: '/id:user_id',
-      name: 'profile',
-      component: HelloWorld
-    },
-    {
-      path: '/chats',
-      name: 'chats',
-      component: HelloWorld
-    }
-  ]
+  mode: 'history',
+  routes: []
 })
